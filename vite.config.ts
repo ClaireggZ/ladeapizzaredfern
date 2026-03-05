77a6d5954bc3a24b36e2449ef5eca1d94f927e5e
Eggy <ClaireZ888@outlook.com>
2026-03-05 15:40:31 +1100

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ladeapizzaredfern/',
   server: {
-    base: '/ladeapizzaredfern/',
     host: "::",
     port: 8080,
     hmr: {
